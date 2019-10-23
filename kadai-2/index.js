45008243df1986dff1e1f18f1e6325e5b727866a
hikari0815 <cheriedayo0229@gmail.com>
2019-10-23 05:16:27 +0000

    Some fix

---
+++ kadai-2/index.js
@@ -0,0 +1,14 @@
+const firstname = 'Hikari'
+const secondname = 'Kobayashi'
+console.log(firstname + secondname)
+
+const sum =1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+console.log(sum)
+
+const hasCat = false
+const hasDog = true
+
+console.log(hasCat || hasDog)
+console.log(hasCat && hasDOG)
+ // 犬も猫も飼っていない
+console.log(!hasCat && !hasDog)
