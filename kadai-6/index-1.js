82ce59143951895d9e3cef969477c36d08481387
hikari0815 <56061110+hikari0815@users.noreply.github.com>
2020-01-31 11:54:07 +0900

    Update index-1.js

--- kadai-6/index-1.js
+++ kadai-6/index-1.js
@@ -1,13 +1,12 @@
 const obj1 = "abc" 
-2 const obj2 = 123 
-3 const obj3 = false 
-4 const obj4 = { a: 123 } 
-5 const obj5 = { func: () => console.log('Hi!') } 
-6 const obj6 = { property: undefined } 
-7 const obj7 = {a: "aaa", b: { c: "ccc", d: undefined } } 
-8 
- 
-9 console.log(JSON.stringify(obj1)) 
-10 console.log(JSON.stringify(obj2)) 
-11 console.log(JSON.stringify(obj5)) 
-12 console.log(JSON.stringify(obj7)) 
+const obj2 = 123 
+const obj3 = false 
+const obj4 = { a: 123 } 
+const obj5 = { func: () => console.log('Hi!') } 
+const obj6 = { property: undefined } 
+const obj7 = {a: "aaa", b: { c: "ccc", d: undefined } } 
+
+console.log(JSON.stringify(obj1)) 
+console.log(JSON.stringify(obj2)) 
+console.log(JSON.stringify(obj5)) 
+console.log(JSON.stringify(obj7)) 
